af4d736dfbd2c0a121aa44f6e1d1fadae3d37260
MATHAWEE <mathawee@MacBook-Pro-khxng-MATHAWEE.local>
2019-03-14 16:37:40 +0700

    create App component

---
+++ component/App.js
@@ -0,0 +1,16 @@
+// import Library
+import React from 'react';
+import { View, Text } from 'react-native';
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World!!</Text>
+            </View>
+        )
+    }
+}
+
+//export
+export default App;
