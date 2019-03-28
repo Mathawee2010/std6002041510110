062d6809e9bf1ae6c5b8c637af787a1b14272396
MATHAWEE <mathawee@MacBook-Pro-khxng-MATHAWEE.local>
2019-03-28 14:53:34 +0700

    StyleText

--- component/App.js
+++ component/App.js
@@ -5,7 +5,7 @@ import Header from './Header.js';
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View>
         <Header title="Albums" />
         <Text>Mathawee Phuto #TCT27</Text>
       </View>
