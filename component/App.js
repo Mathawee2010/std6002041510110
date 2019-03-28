3d1ba6f75ca60bcfdb81666960257fdfc646420a
MATHAWEE <mathawee@MacBook-Pro-khxng-MATHAWEE.local>
2019-03-28 14:28:26 +0700

    header

--- component/App.js
+++ component/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Header from './Header.js';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Mathawee Phuto</Text>
+        <Header title="Albums" />
+        <Text>Mathawee Phuto #TCT27</Text>
       </View>
     );
   }
