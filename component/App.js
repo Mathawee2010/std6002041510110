d8e12113b0786c2355dd16def612cb606bb73e48
MATHAWEE <mathawee@MacBook-Pro-khxng-MATHAWEE.local>
2019-04-04 17:15:48 +0700

    email and password

--- component/App.js
+++ component/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import Header from './Header.js';
 import Card from './Card'
+import Login from './Login';
 
 class App extends React.Component {
   render() {
     return (
       <View>
-        <Header title="Albums" />
-        <Text></Text>
-        <Card />
+        <Header title="Login" />
+        <Login />
       </View>
     );
   }
